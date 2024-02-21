3b59eb35ce2db6f85ba1e30a1b421e1dd77ebfce
Bek_Ancer <bek.mag.00@mail.ru>
2024-02-22 00:00:30 +0600

    .

---
+++ LAB3/2 task/ToDo App/test.js
@@ -0,0 +1,5 @@
+let a;
+a = 10;
+a = "test";
+a = 10.2;
+console.log(a);
---
+++ LAB4/my-proj/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HomeComponent],
+  template: `
+  <main>
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+`,
+  styleUrl:'./app.component.css',
+})
+export class AppComponent {
+  title = 'homes';
+}
